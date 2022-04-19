d25332363cc1ca76df600c2d2cab7e8419cd4291
James Yeoman <jamesyeoman10@gmail.com>
2022-04-19 17:23:44 +0100

    Restructure envs to make path easier

--- plugins/envs/envs.plugin.zsh
+++ plugins/envs/envs.plugin.zsh
@@ -1,20 +1,14 @@
 function exportEnvIfExists() {
     if [[ -d "$2" ]]; then
-        eval "export ${1}=\"${2}/${3}\""
-        lazyload "$3" -- 'eval "$('$3' init -)"'
+        eval "export ${1:u}_ROOT=\"${2}/${1}\""
+        lazyload "$1" -- 'eval "$('$1' init -)"'
     fi
 }
 
-XDG_COMPLIANT="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XENV_COMMANDS=("jenv" "pyenv" "nodenv" "sbtenv" "scalaenv" "goenv" "rbenv" "phpenv")
 
-exportEnvIfExists "JENV_ROOT" "${XDG_COMPLIANT}" "jenv"
-exportEnvIfExists "PYENV_ROOT" "${XDG_COMPLIANT}" "pyenv"
-exportEnvIfExists "NODENV_ROOT" "${XDG_COMPLIANT}" "nodenv"
-exportEnvIfExists "SBTENV_ROOT" "${XDG_COMPLIANT}" "sbtenv"
-exportEnvIfExists "SCALAENV_ROOT" "${XDG_COMPLIANT}" "scalaenv"
-exportEnvIfExists "GOENV_ROOT" "${XDG_COMPLIANT}" "goenv"
-exportEnvIfExists "RBENV_ROOT" "${XDG_COMPLIANT}" "rbenv"
-exportEnvIfExists "PHPENV_ROOT" "${XDG_COMPLIANT}" "phpenv"
+for cmnd in $XENV_COMMANDS; do
+    exportEnvIfExists "$cmnd" "${XDG_DATA_HOME:-$HOME/.local/share}"
+done
 
 unset exportEnvIfExists
-unset XDG_COMPLIANT
